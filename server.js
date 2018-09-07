d17f1bc8238a65eaf7c4ac277e178d730174499d
klhong124 <klhong124@gmail.com>
2018-09-07 16:02:33 +0800

    RTO ~

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+var express = require("express");
+var path    = require("path");
+var app     = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000);
+console.log("Running at Port 3000");
