4803ef8bf6491289b24c7c32442c9ddaeedda7b5
QI <qi@QIs-MacBook-Air.local>
2018-09-10 23:11:53 +0800

    change to ejs layouts

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var express = require("express");
-var path    = require("path");
-var app     = express();
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
 });
 
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about', {text: "test"});
+});
+
+// signin page
+app.get('/signin', function(req, res) {
+    res.render('pages/sign-in');
+});
+
+app.use('/static', express.static('public'))
 app.listen(3000);
-console.log("Running at Port 3000");
+console.log("The port is 3000");
