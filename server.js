a4cd32b9f27521e0c60f3e461d625d3139f4f9ab
klhong124 <klhong124@gmail.com>
2018-09-21 22:47:56 +0800

    dbconn should within app

--- server.js
+++ server.js
@@ -5,37 +5,13 @@ var app = express();
 app.set('view engine', 'ejs');
 // oracle db config
 const oracledb = require('oracledb');
-const config = {
+ config = {
   user: 'G1_team001',                // Update me
   password: 'ceG1_team001',        // Update me
   connectString: '144.214.177.102/xe'   // Update me
 };
+const conn = oracledb.getConnection(config);
 
-
-async function auth(email, username,res) {
-    let conn;
-
-    conn = await oracledb.getConnection(config);
-
-    const result = await conn.execute(
-      'select * from users where email = :email and username = :username',
-      [email, username]
-      // 'select * from users'
-    );
-    if (result.rows.length > 0){
-        console.log("ok");
-    }else{
-        console.log("err");
-    }
-    // console.log(result.rows)
-    // conn assignment worked, need to close
-    await conn.close();
-}
-// call function run
-// auth();
-
-
-// auth
 var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
 // var router = express.Router();
 
@@ -44,21 +20,6 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-passport.use(new LocalStrategy(
-  function(email, password, done) {
-      User.findOne({ email: email }, function(err, user) {
-          if (err) { return done(err); }
-          if (!user) {
-              return done(null, false, { message: 'Incorrect email.' });
-          }
-          if (!user.validPassword(password)) {
-              return done(null, false, { message: 'Incorrect password.' });
-          }
-          return done(null, user);
-      });
-  }
-));
 
 passport.use(new LocalStrategy({
     emailField: 'email',
@@ -72,12 +33,12 @@ passport.use(new LocalStrategy({
 
 // use res.render to load up an ejs view file
 // index page
-app.get('/', function(req, res) {
+app.get('/',(req, res)=> {
     res.render('pages/index');
 });
 
 // about page
-app.get('/about', function(req, res) {
+app.get('/about',(req, res)=> {
     res.render('pages/about', {output: req.params.id});
 });
 
@@ -88,49 +49,65 @@ app.get('/about', function(req, res) {
 //     res.redirect('/');
 //   });
 // POST /login gets urlencoded bodies
-app.post('/login', urlencodedParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400)
-  auth(req.body.email, req.body.password);
-  console.log("------");
-  console.log(req.body);
+app.post('/login', urlencodedParser, (req, res) => {
+  oracledbconn(req.body.email,req.body.password)
+    async function oracledbconn(email,password)  {
 
-  console.log("++++++");
+    let conn;
 
+    try {
+      conn = await oracledb.getConnection(config);
+
+      const result = await conn.execute(
+        'select * from users where email = :email and username = :username',
+        [email,password]// 'select * from users'
+      );
+
+      console.log(result.rows[0]);
+    } catch (err) {
+      console.log('Ouch!', err);
+    } finally {
+      if (conn) { // conn assignment worked, need to close
+         await conn.close();
+      }
+    }
+    res.redirect('/dashboard');
+  };
 })
 
 
 // signup page
-app.get('/register', function(req, res) {
+app.get('/register',(req, res) => {
     res.render('pages/signup');
 });
 
 // dashboard page
-app.get('/dashboard', function(req, res) {
+app.get('/dashboard',(req, res) => {
     res.render('pages/dashboard');
 });
 
 // signout
-app.get('/logout', function(req, res) {
+app.get('/logout',(req, res) => {
     res.redirect('/');
 });
 
 // change password page
-app.get('/changepassword', function(req, res) {
+app.get('/changepassword',(req, res) => {
     res.render('pages/change-password');
 });
 
 // all categories page
-app.get('/products', function(req, res) {
+app.get('/products',(req, res) => {
     res.render('pages/products');
 });
 
 // checkout form page
-app.get('/checkout', function(req, res) {
+app.get('/checkout',(req, res) => {
     res.render('pages/checkout');
 });
 
 // forget password page
-app.get('/forgetpassword', function(req, res) {
+app.get('/forgetpassword',(req, res) => {
     res.render('pages/forget-password');
 });
 
